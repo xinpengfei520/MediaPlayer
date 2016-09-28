521a49157ed1b103fef90425f99c18ce18578950
xinpengfei520 <542270559@qq.com>
2016-09-28 21:02:14 +0800

    five
    设置RadioGroup状态改变的监听

--- app/src/main/java/com/atguigu/mediaplayer/MainActivity.java
+++ app/src/main/java/com/atguigu/mediaplayer/MainActivity.java
@@ -1,13 +1,108 @@
 package com.atguigu.mediaplayer;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.RadioGroup;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+
+import fragment.AudioFragment;
+import fragment.BaseFragment;
+import fragment.NetAudioFragment;
+import fragment.NetVideoFragment;
+import fragment.VideoFragment;
+
+public class MainActivity extends FragmentActivity {
+
+    private RadioGroup rg_main;
+
+    /**
+     * 存放各个子Fragemnt页面的集合
+     */
+    private ArrayList<BaseFragment> fragments;
+
+    /**
+     * 列表中对应的Fragment的位置
+     */
+    private int position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rg_main = (RadioGroup)findViewById(R.id.rg_main);
+
+        initFragment();
+
+        /**
+         * 设置RadioGroup状态改变的监听
+         */
+        rg_main.setOnCheckedChangeListener(new MyOncheckedChangeListener());
+    }
+
+    class MyOncheckedChangeListener implements RadioGroup.OnCheckedChangeListener{
+
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+            switch (checkedId) {
+                case R.id.rb_main_video :
+                    position = 0;
+                    break;
+                case R.id.rb_main_music :
+                    position = 1;
+                    break;
+                case R.id.rb_net_video :
+                    position = 2;
+                    break;
+                case R.id.rb_net_music :
+                    position = 3;
+                    break;
+            }
+
+            Fragment toFragment = getFragment(position);
+            switchFragment(toFragment);
+        }
+    }
+
+    private void switchFragment(Fragment toFragment) {
+        Fragment fragment = getFragment(0);
+        //1.得到FragmentManager
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        //2.开启事务
+        FragmentTransaction transaction = supportFragmentManager.beginTransaction();
+
+        //3.替换
+        transaction.replace(R.id.fl_main_container,toFragment);
+
+        //4.提交事务
+        transaction.commit();
+    }
+
+    /**
+     * 得到Fragment: 从集合中取出相应位置的fragment
+     * @param position
+     * @return
+     */
+    private Fragment getFragment(int position) {
+        return fragments.get(position);
+
+    }
+
+    /**
+     * 初始化Fragment
+     */
+    private void initFragment() {
+
+        fragments = new ArrayList<>();
+        fragments.add(new VideoFragment());//本地视频
+        fragments.add(new AudioFragment());//本地音频
+        fragments.add(new NetVideoFragment());//网络视频
+        fragments.add(new NetAudioFragment());//网络音频
+
     }
 }
