a2dcd4504d4a979dad916eb1232d8a55ab01f056
xinpengfei520 <542270559@qq.com>
2016-10-08 19:11:50 +0800

    18th
    网络视频的布局文件

--- app/src/main/java/fragment/NetVideoFragment.java
+++ app/src/main/java/fragment/NetVideoFragment.java
@@ -1,10 +1,12 @@
 package fragment;
 
-import android.graphics.Color;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.atguigu.mediaplayer.R;
 
 /**
  * Created by xinpengfei on 2016/9/28.
@@ -15,18 +17,27 @@ import android.widget.TextView;
 public class NetVideoFragment extends BaseFragment {
 
     private TextView textView;
+    private ListView listview;
+    private ProgressBar progressbar;
+    private TextView tv_nodata;
 
     @Override
     public View initView() {
 
         Log.e("TAG", "网络视频UI创建了");
 
-        textView = new TextView(context);
-        textView.setTextSize(25);
-        textView.setTextColor(Color.RED);
-        textView.setGravity(Gravity.CENTER);
+//        textView = new TextView(context);
+//        textView.setTextSize(25);
+//        textView.setTextColor(Color.RED);
+//        textView.setGravity(Gravity.CENTER);
+//
+//        return textView;
 
-        return textView;
+        View view = View.inflate(context, R.layout.fragment_netvideo, null);
+        listview = (ListView) view.findViewById(R.id.listview);
+        progressbar = (ProgressBar) view.findViewById(R.id.progressbar);
+        tv_nodata = (TextView) view.findViewById(R.id.tv_nodata);
+        return view;
 
     }
 
@@ -34,6 +45,6 @@ public class NetVideoFragment extends BaseFragment {
     public void initData() {
         super.initData();
         Log.e("TAG", "网络视频数据绑定了");
-        textView.setText("网络视频的内容");
+//        textView.setText("网络视频的内容");
     }
 }
