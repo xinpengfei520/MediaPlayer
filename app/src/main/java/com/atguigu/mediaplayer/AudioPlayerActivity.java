2a32284a48ce57d4e92ad0310169d2ab66d78e6d
xinpengfei520 <542270559@qq.com>
2016-10-09 22:29:51 +0800

    31th
    创建服务和创建对应的aidl文件;
    音乐的暂停和播放;

--- app/src/main/java/com/atguigu/mediaplayer/AudioPlayerActivity.java
+++ app/src/main/java/com/atguigu/mediaplayer/AudioPlayerActivity.java
@@ -1,22 +1,155 @@
 package com.atguigu.mediaplayer;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class AudioPlayerActivity extends Activity {
+import service.MusicPlayerService;
+
+public class AudioPlayerActivity extends Activity implements View.OnClickListener {
 
     private ImageView iv_icon;
+    private int position;
+    private TextView tvArtist;
+    private TextView tvName;
+    private TextView tvTime;
+    private SeekBar seekbarAudio;
+    private Button btnAudioPlaymode;
+    private Button btnAudioPre;
+    private Button btnAudioStartPause;
+    private Button btnAudioNext;
+    private Button btnAudioSwichLyricCover;
+    /**
+     * 服务的代理类-aidl文件动态生成的类
+     */
+    private IMusicPlayerService service;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio_player);
 
+        findViews();
+        getData();
+        startAndBindService();
+//        iv_icon = (ImageView) findViewById(R.id.iv_icon);
+//        iv_icon.setBackgroundResource(R.drawable.animation_list);
+//        AnimationDrawable animationDrawable = (AnimationDrawable) iv_icon.getBackground();
+//        animationDrawable.start();
+    }
+
+    private void startAndBindService() {
+        Intent intent = new Intent(this, MusicPlayerService.class);
+        intent.setAction("com.atguigu.mobileplayer.OPENAUDIO");
+        bindService(intent, conn, Context.BIND_AUTO_CREATE);
+        startService(intent);//防止服务多次实例化
+    }
+
+    private void getData() {
+        position = getIntent().getIntExtra("position", 0);
+    }
+
+    private void findViews() {
+        setContentView(R.layout.activity_audio_player);
+        tvArtist = (TextView) findViewById(R.id.tv_artist);
+        tvName = (TextView) findViewById(R.id.tv_name);
+        tvTime = (TextView) findViewById(R.id.tv_time);
+        seekbarAudio = (SeekBar) findViewById(R.id.seekbar_audio);
+        btnAudioPlaymode = (Button) findViewById(R.id.btn_audio_playmode);
+        btnAudioPre = (Button) findViewById(R.id.btn_audio_pre);
+        btnAudioStartPause = (Button) findViewById(R.id.btn_audio_start_pause);
+        btnAudioNext = (Button) findViewById(R.id.btn_audio_next);
+        btnAudioSwichLyricCover = (Button) findViewById(R.id.btn_audio_swich_lyric_cover);
         iv_icon = (ImageView) findViewById(R.id.iv_icon);
         iv_icon.setBackgroundResource(R.drawable.animation_list);
         AnimationDrawable animationDrawable = (AnimationDrawable) iv_icon.getBackground();
         animationDrawable.start();
+
+        btnAudioPlaymode.setOnClickListener(this);
+        btnAudioPre.setOnClickListener(this);
+        btnAudioStartPause.setOnClickListener(this);
+        btnAudioNext.setOnClickListener(this);
+        btnAudioSwichLyricCover.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnAudioPlaymode) {
+            // Handle clicks for btnAudioPlaymode
+        } else if (v == btnAudioPre) {
+            // Handle clicks for btnAudioPre
+        } else if (v == btnAudioStartPause) {
+            try {
+                if (service.isPlaying()) {
+                    //暂停
+
+                    service.pause();
+                    //按钮设置-播放状态
+                    btnAudioStartPause.setBackgroundResource(R.drawable.btn_audio_start_selector);
+                } else {
+                    //播放
+                    service.start();
+                    //按钮设置-暂停
+                    btnAudioStartPause.setBackgroundResource(R.drawable.btn_audio_pause_selector);
+                }
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+            // Handle clicks for btnAudioStartPause
+        } else if (v == btnAudioNext) {
+            // Handle clicks for btnAudioNext
+        } else if (v == btnAudioSwichLyricCover) {
+            // Handle clicks for btnAudioSwichLyricCover
+        }
+    }
+
+    private ServiceConnection conn = new ServiceConnection() {
+
+        /**
+         * 当和服务连接成功的时候回调
+         * @param name
+         * @param binder
+         */
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder binder) {
+
+            service = IMusicPlayerService.Stub.asInterface(binder);//得到服务代理类
+
+            //开始播放音乐
+            try {
+                service.openAudio(position);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * 当断开服务的时候回调
+         * @param name
+         */
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        if (conn != null) {
+            unbindService(conn);
+            conn = null;
+        }
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/service/MusicPlayerService.java
@@ -0,0 +1,298 @@
+package service;
+
+import android.app.Service;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+import com.atguigu.mediaplayer.IMusicPlayerService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import domain.MediaItem;
+
+//import android.support.annotation.Nullable;
+
+/**
+ * Created by xinpengfei on 2016/10/9.
+ * <p>
+ * Function :播放音乐的服务,里面封装音乐的逻辑
+ */
+
+public class MusicPlayerService extends Service {
+
+
+    private ArrayList<MediaItem> mediaItems;
+    /**
+     * 这个音频的信息
+     */
+    private MediaItem mediaItem;
+    /**
+     * 当前播放列表中的位置
+     */
+    private int position;
+
+    private MediaPlayer mediaPlayer;
+
+
+    private IMusicPlayerService.Stub stub = new IMusicPlayerService.Stub() {
+
+        MusicPlayerService service = MusicPlayerService.this;//得到外面服务的实例
+
+        @Override
+        public void openAudio(int position) throws RemoteException {
+            service.openAudio(position);//调用服务的openAudio
+        }
+
+        @Override
+        public void start() throws RemoteException {
+            service.start();
+        }
+
+        @Override
+        public void pause() throws RemoteException {
+            service.pause();
+        }
+
+        @Override
+        public void setPlaymode(int playmode) throws RemoteException {
+            service.setPlaymode(playmode);
+        }
+
+        @Override
+        public int getPlaymode() throws RemoteException {
+            return service.getPlaymode();
+        }
+
+        @Override
+        public String getArtist() throws RemoteException {
+            return service.getArtist();
+        }
+
+        @Override
+        public String getAudioName() throws RemoteException {
+            return service.getAudioName();
+        }
+
+        @Override
+        public int getDuration() throws RemoteException {
+            return service.getDuration();
+        }
+
+        @Override
+        public int getCurrentPosition() throws RemoteException {
+            return service.getCurrentPosition();
+        }
+
+        @Override
+        public void seekTo(int position) throws RemoteException {
+            service.seekTo(position);
+        }
+
+        @Override
+        public void next() throws RemoteException {
+            service.next();
+        }
+
+        @Override
+        public void pre() throws RemoteException {
+            service.pre();
+        }
+
+        @Override
+        public boolean isPlaying() throws RemoteException {
+            return mediaPlayer.isPlaying();
+        }
+
+
+    };
+
+    private void pre() {
+
+    }
+
+    private void next() {
+
+    }
+
+    /**
+     * 音频的拖动
+     * @param position
+     */
+    private void seekTo(int position) {
+
+    }
+
+    /**
+     * 得到当前的播放进度
+     * @return
+     */
+    private int getCurrentPosition() {
+        return 0;
+    }
+
+    /**
+     * 得到音频的总时长
+     * @return
+     */
+    private int getDuration() {
+        return 0;
+    }
+
+    private String getAudioName() {
+        return "";
+    }
+
+    private String getArtist() {
+        return "";
+    }
+
+    private int getPlaymode() {
+        return 0;
+    }
+
+    private void setPlaymode(int playmode) {
+
+    }
+
+    private void pause() {
+        mediaPlayer.pause();
+    }
+
+    private void start() {
+        mediaPlayer.start();
+    }
+
+    private void openAudio(int position) {
+        if (mediaItems != null && mediaItems.size() > 0) {
+            this.position = position;
+            if (position < mediaItems.size()) {
+                mediaItem = mediaItems.get(position);
+            }
+
+            //释放MediaPlayer
+            if (mediaPlayer != null) {
+                mediaPlayer.reset();
+                mediaPlayer.release();
+            }
+            try {
+                //创建MediaPlayer,重新设置监听,重新准备
+                mediaPlayer = new MediaPlayer();
+                mediaPlayer.setOnPreparedListener(new MyOnPreparedListener());
+                mediaPlayer.setOnCompletionListener(new MyOnCompletionListener());
+                mediaPlayer.setOnErrorListener(new MyOnErrorListener());
+                //设置播放地址
+                mediaPlayer.setDataSource(mediaItem.getData());
+
+                mediaPlayer.prepareAsync();//准备
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        } else {
+            Toast.makeText(MusicPlayerService.this, "音频还没有加载完成", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    class MyOnErrorListener implements MediaPlayer.OnErrorListener {
+
+        @Override
+        public boolean onError(MediaPlayer mp, int what, int extra) {
+            next();
+            return true;
+        }
+    }
+
+    class MyOnPreparedListener implements MediaPlayer.OnPreparedListener {
+
+        @Override
+        public void onPrepared(MediaPlayer mp) {
+            //开始播放
+            start();
+        }
+    }
+
+    class MyOnCompletionListener implements MediaPlayer.OnCompletionListener {
+
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            //播放完成的时候播放下一个
+            next();
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        getData();
+    }
+
+    private void getData() {
+
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+
+                mediaItems = new ArrayList<MediaItem>();
+                ContentResolver resolver = getContentResolver();
+                Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                String[] objs = new String[]{
+                        MediaStore.Audio.Media.DISPLAY_NAME,//在sdcard时候的名称
+                        MediaStore.Audio.Media.DURATION,//视频的时长，毫秒
+                        MediaStore.Audio.Media.SIZE,//文件大小，单位字节
+                        MediaStore.Audio.Media.ARTIST,//演唱者
+                        MediaStore.Audio.Media.DATA//在sdcard上路径
+                };
+                Cursor cursor = resolver.query(uri, objs, null, null, null);
+                if (cursor != null) {
+
+
+                    //循环
+                    while (cursor.moveToNext()) {
+
+                        //创建了一个视频信息类
+                        MediaItem mediaItem = new MediaItem();
+                        //添加到集合中
+                        mediaItems.add(mediaItem);
+
+                        String name = cursor.getString(0);
+                        mediaItem.setName(name);
+                        long duration = cursor.getLong(1);
+                        mediaItem.setDuration(duration);
+                        long size = cursor.getLong(2);
+                        mediaItem.setSize(size);
+                        String artist = cursor.getString(3);
+                        mediaItem.setArtist(artist);
+                        String data = cursor.getString(4);
+                        mediaItem.setData(data);
+
+                    }
+
+                    cursor.close();//关闭
+
+                }
+
+                //发消息
+                //handler.sendEmptyMessage(0);
+            }
+        }.start();
+    }
+
+    //千万不能忘记
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return stub;
+    }
+}
