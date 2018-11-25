e213c6a5df2aafd5352a28a526f54bc119527e24
xinpengfei <542270559@qq.com>
2018-11-25 22:37:46 +0800

    adapter Android Studio 3.2 & update dependency library.

--- speechDemo/build/generated/source/buildConfig/debug/com/iflytek/voicedemo/BuildConfig.java
+++ speechDemo/build/generated/source/buildConfig/debug/com/iflytek/voicedemo/BuildConfig.java
@@ -9,5 +9,5 @@ public final class BuildConfig {
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "2.0.1018.1013";
+  public static final String VERSION_NAME = "1.0";
 }
